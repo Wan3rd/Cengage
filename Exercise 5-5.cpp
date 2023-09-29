a592bba445e9ef07014ae33a96514d26e7709bd1
Wan3rd <WanTedSpexz@gmail.com>
2023-09-29 15:02:50 +0800

    fix

--- Exercise 5-5.cpp
+++ Exercise 5-5.cpp
@@ -5,33 +5,37 @@
 
 using namespace std;
 
-int main() {
-    int num1, num2;
-    int Multiples_of_3 = 0;
-    int Multiples_of_5 = 0;
+int main() 
+{
+    //Declaration of variables
+    int num1, num2, Multiples_of_3 = 0, Multiples_of_5 = 0; 
 
-    cout << "Enter the first number: ";
-    cin >> num1;
+    cout<<"Enter the first number: ";
+    cin>>num1;
 
     cout << "Enter the second number: ";
-    cin >> num2;
+    cin>>num2;
 
+    //if num1 is larger it will swap to num2
     if (num1 > num2) {
         swap(num1, num2);
     }
 
-    for (int i = num1; i <= num2; ++i) {
-        if (i % 3 == 0) {
+    for (int i = num1; i <= num2; ++i) 
+    {
+        if (i % 3 == 0) 
+        {
             Multiples_of_3++;
         }
 
-        if (i % 5 == 0) {
+        if (i % 5 == 0) 
+        {
             Multiples_of_5++;
         }
     }
 
-    cout <<Multiples_of_3 << endl;
-    cout <<Multiples_of_5 << endl;
+    cout<<Multiples_of_3 << endl;
+    cout<<Multiples_of_5 << endl;
 
     return 0;
 }
